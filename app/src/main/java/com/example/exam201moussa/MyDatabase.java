d7b4263e3376f9d156bda3371bc963accafe4040
medohamss <moussamohamadyassine@gmail.com>
2022-06-03 15:25:31 +0100

    Q4

--- app/src/main/java/com/example/exam201moussa/MyDatabase.java
+++ app/src/main/java/com/example/exam201moussa/MyDatabase.java
@@ -3,4 +3,10 @@ package com.example.exam201moussa;
 public class MyDatabase {
     public static String DB_NAME = "societes.db";
     public static String TABLE_NAME = "societe";
+    public static String COL1 = "ID";
+    public static String COL2 = "Raison_Sociale";
+    public static String COL3 = "Secteur_activite";
+    public static String COL4 = "nb_employe";
+
+
 }
