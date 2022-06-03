00b8702d794d19126685b5133be026be7bc76d82
medohamss <moussamohamadyassine@gmail.com>
2022-06-03 16:05:36 +0100

    Q7

---
+++ app/src/main/java/com/example/exam201moussa/EditSociete.java
@@ -0,0 +1,4 @@
+package com.example.exam201moussa;
+
+public class EditSociete {
+}
--- app/src/main/java/com/example/exam201moussa/MainActivity.java
+++ app/src/main/java/com/example/exam201moussa/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.exam201moussa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1,b2,b3,b4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +16,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void accederer(View view) {
+    public void accederer(View v ) {
+        Intent i=null;
+        switch (v.getId()){
+            case R.id.btnAdd:
+                i=new Intent(this,AddSociete.class);
+                break;
+            case R.id.btnEdit:
+                i=new Intent(this,EditSociete.class);
+                break;
+            case R.id.btnAllSoc:
+                i=new Intent(this,ListeSociete.class);
+                break;
+
+        }
+        startActivity(i);
     }
 }
